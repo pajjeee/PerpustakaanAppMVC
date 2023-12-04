2b7fb4fa68e2e0dbefe1f0aaaf323db81d9147ed
fazarullah <fazarullah@students.amikom.ac.id>
2023-12-04 19:05:36 +0700

    Add project files.

---
+++ PerpustakaanAppMVC/View/FrmEntryMahasiswa.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PerpustakaanAppMVC.View
+{
+    partial class FrmEntryMahasiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNpm = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAngkatan = new System.Windows.Forms.TextBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnSelesai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NPM";
+            // 
+            // txtNpm
+            // 
+            this.txtNpm.Location = new System.Drawing.Point(142, 23);
+            this.txtNpm.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtNpm.Name = "txtNpm";
+            this.txtNpm.Size = new System.Drawing.Size(144, 31);
+            this.txtNpm.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(142, 73);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(568, 31);
+            this.txtNama.TabIndex = 1;
+            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Angkatan";
+            // 
+            // txtAngkatan
+            // 
+            this.txtAngkatan.Location = new System.Drawing.Point(142, 123);
+            this.txtAngkatan.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtAngkatan.Name = "txtAngkatan";
+            this.txtAngkatan.Size = new System.Drawing.Size(144, 31);
+            this.txtAngkatan.TabIndex = 2;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(402, 177);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(150, 44);
+            this.btnSimpan.TabIndex = 3;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnSelesai
+            // 
+            this.btnSelesai.Location = new System.Drawing.Point(564, 177);
+            this.btnSelesai.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSelesai.Name = "btnSelesai";
+            this.btnSelesai.Size = new System.Drawing.Size(150, 44);
+            this.btnSelesai.TabIndex = 4;
+            this.btnSelesai.Text = "Selesai";
+            this.btnSelesai.UseVisualStyleBackColor = true;
+            // 
+            // FrmEntryMahasiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 238);
+            this.Controls.Add(this.btnSelesai);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtAngkatan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNpm);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmEntryMahasiswa";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmEntryMahasiswa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNpm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAngkatan;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnSelesai;
+    }
+}
